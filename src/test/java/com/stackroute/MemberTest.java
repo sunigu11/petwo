f1aa6fa291db27af5dbe6f2bd262484cc7d15f14
sunigu11 <sunilgupta0739@gmail.com>
2018-12-21 10:43:59 +0530

    2nd test case added

--- src/test/java/com/stackroute/MemberTest.java
+++ src/test/java/com/stackroute/MemberTest.java
@@ -1,6 +1,7 @@
 package com.stackroute;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,10 +14,24 @@ public class MemberTest {
         member = new Member("Harry Potter",30,2500.3);
     }
 
+    @Test
     public void testDisplay(){
 
         //act
         boolean expected = true;
+
+        //arrange
+        boolean result = member.display();
+
+        //assert
+        assertTrue(expected,result);
+    }
+
+    @Test
+    public void testDisplay(){
+
+        //act
+        boolean expected = false;
 
         //arrange
         boolean result = member.display();
